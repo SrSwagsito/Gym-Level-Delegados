1e0153a6914371f982c15a1359944cb1d29ec139
SrSwagsito <andres12felipe34@hotmail.com>
2024-11-08 21:52:46 -0500

    Initial commit

---
+++ Assets/Scripts/ParallaxMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMovement : MonoBehaviour
+{
+    [SerializeField] private Vector2 movementSpeed;
+
+    private Vector2 offSet;
+
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        offSet = movementSpeed * Time.deltaTime;
+        material.mainTextureOffset += offSet;
+    }
+}
